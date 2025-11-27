0c7b3f118d25f9b2a36594ba33370c8897be2123
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 15:17:43 +0530

    Fix formatting issues in temperature classification script

--- temp.py
+++ temp.py
@@ -1,12 +1,15 @@
 import sys
-if len(sys.argv)!=2 :
- print("usage : python script.py <temp>")
- exit(1)
-script_name=sys.argv[0]
-temp=float(sys.argv[1])
-if temp<=15 :
-  print("cold")
-elif temp>=15 and temp<=30:
-  print("moderate")
-else :
- print("hot")
+
+if len(sys.argv) != 2:
+    print("usage : python script.py <temp>")
+    exit(1)
+
+script_name = sys.argv[0]
+temp = float(sys.argv[1])
+
+if temp <= 15:
+    print("cold")
+elif temp > 15 and temp <= 30:
+    print("moderate")
+else:
+    print("hot")
