ecf7004ce12be2ce0d84875a6f5009a2b46cd506
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 15:13:08 +0530

    Convert input temperature to float for comparison

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ if len(sys.argv)!=2 :
  print("usage : python script.py <temp>")
  exit(1)
 script_name=sys.argv[0]
-temp=sys.argv[1]
+temp=float(sys.argv[1])
 if temp<15 :
   print("cold")
 elif temp>15 and temp<30:
