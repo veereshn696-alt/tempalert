79206b39c51a19faa33b41876b04591f0abb398b
veereshn696-alt <veereshn696@gmail.com>
2025-11-27 15:16:01 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,9 +4,9 @@ if len(sys.argv)!=2 :
  exit(1)
 script_name=sys.argv[0]
 temp=float(sys.argv[1])
-if temp<15 :
+if temp<=15 :
   print("cold")
-elif temp>=15 and temp<30:
+elif temp>=15 and temp<=30:
   print("moderate")
 else :
  print("hot")
